70f840b6a1da9fa87fc28f401903e155c3790f9f
prajogt <113074010+prajogt@users.noreply.github.com>
2024-03-12 00:11:18 -0400

    Download the desired data

---
+++ scripts/01_download_data.R
@@ -0,0 +1,37 @@
+#### Preamble ####
+# Purpose: Download US CES
+# Author: Timothius Prajogi, Janssen Myer Rambaud
+# Date: 23 January 2024
+# Contact: tim.prajogi@mail.utoronto.ca
+# License: MIT
+# Prerequisites: none
+
+#### Workplace setup ####
+
+library(tidyverse)
+library(dataverse)
+
+#### Download and save CES data ####
+
+ces2022 <-
+  get_dataframe_by_name(
+    filename = "CCES22_Common_OUTPUT_vv_topost.csv",
+    dataset = "10.7910/DVN/PR4L8P",
+    server = "dataverse.harvard.edu",
+    .f = read_csv
+  ) |>
+  select(votereg, presvote20post, birthyr, gender4, educ, race, urbancity, religpew, gunown, edloan)
+  # votereg       => If they are registered to vote 
+  # presvote20post => Who they voted for for President during the 2020 lecture
+  # birthyr       => Birth year
+  # gender4       => Identified gender
+  # educ          => Highest level of education completed
+  # race          => Their race
+  # urbancity     => Type of area they live in 
+  # religpew      => Their religion
+  # gunown        => If they our members of their household own a gun
+  # edloan        => If they have education loans
+
+# Save to csv
+write_csv(ces2022, "input/ces2022.csv")
+
