bc6b7faffdbc554a10536f954f0d59082ab062c6
prajogt <113074010+prajogt@users.noreply.github.com>
2024-03-16 21:16:49 -0400

    add column and some data tests

--- scripts/04_test_data.R
+++ scripts/04_test_data.R
@@ -16,3 +16,27 @@ library(tidyverse)
 # Load in data
 ces2022_1 <- read_csv("output/data/cleaned_ces2022_1.csv")
 ces2022_2 <- read_csv("output/data/cleaned_ces2022_2.csv")
+
+
+# Test that column types are as expected
+expect_equal(class(ces2022_1$voted_for), "character")
+expect_equal(class(ces2022_1$age_group), "character")
+expect_equal(class(ces2022_1$gender), "character")
+expect_equal(class(ces2022_1$education), "character")
+expect_equal(class(ces2022_1$race), "character")
+expect_equal(class(ces2022_1$religion), "character")
+
+expect_equal(class(ces2022_2$voted_for), "character")
+expect_equal(class(ces2022_2$age_group), "character")
+expect_equal(class(ces2022_2$gender), "character")
+expect_equal(class(ces2022_2$education), "character")
+expect_equal(class(ces2022_2$race), "character")
+expect_equal(class(ces2022_2$religion), "character")
+expect_equal(class(ces2022_2$household_gun_ownership), "character")
+expect_equal(class(ces2022_2$student_loan_status), "character")
+
+
+# Test that data only contains expected values
+expect_equivalent(sort(unique(ces2022_1$voted_for)), sort(c("Biden", "Trump")))
+expect_equivalent(sort(unique(ces2022_1$age_group)), sort(c("18-29", "30-44", "45-64", "65+")))
+
